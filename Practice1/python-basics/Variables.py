1460930c9b946a85976b4268aa9c5f2fb4e25d34
Alan <zhangir.alan@gmail.com>
2026-01-26 15:55:36 +0500

    Variable Names

--- Practice1/python-basics/Variables.py
+++ Practice1/python-basics/Variables.py
@@ -1,25 +1,12 @@
-x = 5
-y = "John"
-print(x)
-print(y)
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
 
-x = 4       # x is of type int
-x = "Sally" # x is now of type str
-print(x)
+myVariableName = "John"
 
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
-z = float(3)  # z will be 3.0
+MyVariableName = "John"
 
-x = 5
-y = "John"
-print(type(x))
-print(type(y))
-
-x = "John"
-# is the same as
-x = 'John'
-
-a = 4
-A = "Sally"
-#A will not overwrite a
+my_variable_name = "John"
