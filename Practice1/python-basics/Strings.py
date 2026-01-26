fcda94bf8efd276266c045dde51c1189d2000ff2
Alan <zhangir.alan@gmail.com>
2026-01-26 16:14:29 +0500

    Format - Strings

--- Practice1/python-basics/Strings.py
+++ Practice1/python-basics/Strings.py
@@ -1,7 +1,20 @@
-a = "Hello"
-b = "World"
-c = a + b
-print(c)
+age = 36
+#This will produce an error:Type Error
+txt = "My name is John, I am " + age
+print(txt)
 
-c = a + " " + b
-print(c)
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+
+price = 59
+txt = f"The price is {price:.2f} dollars" #The price is 59.00 dollars
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
