f34de938b20ca9004774b1f854a1bba6e0b5fccc
Alan <zhangir.alan@gmail.com>
2026-01-26 16:15:26 +0500

    Escape Characters

--- Practice1/python-basics/Strings.py
+++ Practice1/python-basics/Strings.py
@@ -1,20 +1,2 @@
-age = 36
-#This will produce an error:Type Error
-txt = "My name is John, I am " + age
-print(txt)
+txt = "We are the so-called \"Vikings\" from the north."
 
-age = 36
-txt = f"My name is John, I am {age}"
-print(txt)
-
-price = 59
-txt = f"The price is {price} dollars"
-print(txt)
-
-
-price = 59
-txt = f"The price is {price:.2f} dollars" #The price is 59.00 dollars
-print(txt)
-
-txt = f"The price is {20 * 59} dollars"
-print(txt)
